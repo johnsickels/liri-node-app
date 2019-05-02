e2dcc99630523e47dfdf0632e8a1671606893448
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-05-01 22:39:28 -0400

    beautify results / log them

--- liri.js
+++ liri.js
@@ -9,6 +9,8 @@ var nodeArgs = process.argv;
 var command = process.argv[2];
 var entry = "";
 var fs = require('fs');
+var responseArr = [];
+var cleanResponse = "";
 
 for (var i = 3; i < nodeArgs.length; i++) {
     if (i > 3 && i < nodeArgs.length) {
@@ -19,7 +21,87 @@ for (var i = 3; i < nodeArgs.length; i++) {
     }
 };
 
-liri();
+function concertThis() {
+    axios.get("https://rest.bandsintown.com/artists/" + entry + "/events?app_id=codingbootcamp").then(function (response) {
+
+        for (var i = 0; i < 5; i++) {
+            cleanResponse =
+                "\nVenue: " + response.data[i].venue.name +
+                "\nLocation: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country +
+                "\nDate: " + moment(response.data[i].datetime, "YYYY-MM-DDTHH:mm:ss").format("MM/DD/YYYY");
+
+            console.log(cleanResponse);
+            logResults(cleanResponse);
+        };
+    });
+}
+
+function spotifyThisSong() {
+    spotify
+        .search({ type: 'track', query: entry })
+        .then(function (response) {
+            for (var i = 0; i < 5; i++) {
+            cleanResponse = 
+                "\nArtist(s): " + response.tracks.items[i].artists[0].name +
+                "\nTrack: " + response.tracks.items[i].name +
+                "\nPreview: " + response.tracks.items[i].preview_url +
+                "\nAlbum: " + response.tracks.items[i].album.name;
+            console.log(cleanResponse);
+            logResults(cleanResponse);
+        }
+        })
+        .catch(function () {
+            console.log("Spotify cannot find this song... ");
+        });
+}
+
+function movieThis() {
+    if (entry.length === 0) {
+        entry = "Mr. Nobody";
+    }
+    ;
+    axios.get("http://www.omdbapi.com/?t=" + entry + "&y=&plot=short&apikey=trilogy").then(function (response) {
+        cleanResponse = "\nEntry: " + entry +
+            "\nTitle: " + response.data.Title +
+            "\nRelease Year: " + response.data.Year +
+            "\nIMDB: " + response.data.imdbRating +
+            "\n" + response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value +
+            "\nCountry: " + response.data.Country +
+            "\nLanguage: " + response.data.Language +
+            "\nPlot: " + response.data.Plot +
+            "\nActors: " + response.data.Actors;
+        console.log(cleanResponse);
+        logResults(cleanResponse);
+    })
+        .catch(function (err) {
+            console.log(err);
+        });;
+}
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        };
+        var dataArr = data.split(",");
+        command = dataArr[0];
+        entry = dataArr[1];
+        liri(command, entry);
+    });
+}
+
+function logResults(cleanResponse) {
+    fs.appendFile("log.txt", "\n------------------------------\n" + cleanResponse, function (err) {
+        // If an error was experienced we will log it.
+        if (err) {
+            console.log(err);
+        }
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+        else {
+            console.log("Content Added!");
+        }
+    });
+}
 
 function liri() {
     switch (command) {
@@ -40,56 +122,4 @@ function liri() {
     };
 }
 
-function doWhatItSays() {
-    fs.readFile("random.txt", "utf8", function (error, data) {
-        if (error) {
-            return console.log(error);
-        };
-        var dataArr = data.split(",");
-        command = dataArr[0];
-        entry = dataArr[1];
-        liri(command, entry);
-    });
-}
-
-function movieThis() {
-    if (entry.length === 0) {
-        entry = "Mr. Nobody";
-    }
-    ;
-    axios.get("http://www.omdbapi.com/?t=" + entry + "&y=&plot=short&apikey=trilogy").then(function (response) {
-        console.log("Title: " + response.data.Title);
-        console.log("Release Year: " + response.data.Year);
-        console.log("IMDB: " + response.data.imdbRating);
-        console.log(response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value);
-        console.log("Country: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
-    })
-    .catch(function (err) {
-        console.log(" ");
-    });;
-}
-
-function spotifyThisSong() {
-    spotify
-        .search({ type: 'track', query: entry })
-        .then(function (response) {
-            console.log("Artist(s): " + response.tracks.items[0].artists[0].name);
-            console.log("Track: " + response.tracks.items[0].name);
-            console.log("Preview: " + response.tracks.items[0].preview_url);
-            console.log("Album: " + response.tracks.items[0].album.name);
-        })
-        .catch(function (err) {
-            console.log("Spotify cannot find this song... ");
-        });
-}
-
-function concertThis() {
-    axios.get("https://rest.bandsintown.com/artists/" + entry + "/events?app_id=codingbootcamp").then(function (response) {
-        console.log(response.data[0].venue.name);
-        console.log(response.data[0].venue.city + ", " + response.data[0].venue.region + ", " + response.data[0].venue.country);
-        console.log(moment(response.data[0].datetime, "YYYY-MM-DDTHH:mm:ss").format("MM/DD/YYYY"));
-    });
-}
+liri();
